9a694bd0bea0147b5803d48254104e1a9f57e3e8
rdni <100790587+rdni@users.noreply.github.com>
2023-08-29 20:20:22 +0100

    Make __init__.py more concise

--- hPyT/__init__.py
+++ hPyT/__init__.py
@@ -6,11 +6,13 @@ License - MIT
 Homepage - https://github.com/zingzy/hPyT
 """
 
-from .hPyT import title_bar
-from .hPyT import maximize_minimize_button
-from .hPyT import all_stuffs
-from .hPyT import maximize_button
-from .hPyT import minimize_button
+from .hPyT import (
+    title_bar, 
+    minimize_button, 
+    maximize_button, 
+    all_stuffs, 
+    maximize_minimize_button
+)
 
 __all__ = [
     'title_bar',
